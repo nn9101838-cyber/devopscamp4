cdb22304f067d6efe76d4f19cee0baf9a75368b0
Nandi <nandi@example.com>
2026-03-27 14:42:21 +0530

    Add info.py system script

---
+++ info.py
@@ -0,0 +1,9 @@
+import os
+import socket
+import datetime
+
+print("=== SYSTEM INFO ===")
+print("Time:", datetime.datetime.now())
+print("Hostname:", socket.gethostname())
+print("Current Files:", os.listdir())
+print("User:", os.getlogin())
