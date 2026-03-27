5b7026ff398a1f6e1231c5c302da7cf3cb67fe35
Nandi <nandi@example.com>
2026-03-27 15:30:14 +0530

    Add robust user detection with fallbacks

--- info.py
+++ info.py
@@ -1,10 +1,19 @@
 import os
 import socket
 import datetime
+import getpass
 
 print("=== SYSTEM INFO ===")
 print("Time:", datetime.datetime.now())
 print("Hostname:", socket.gethostname())
 print("Current Files:", os.listdir())
-import getpass
-print("User:", getpass.getuser())
+
+try:
+    user = os.getlogin()
+except Exception:
+    try:
+        user = getpass.getuser()
+    except Exception:
+        user = os.environ.get('USER', 'root')
+
+print("User:", user)
